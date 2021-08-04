05c3673b9eabd6dc04ba98c6bdd0a5a3488b6951
Dennis Iversen <dennis.iversen@gmail.com>
2021-08-04 16:52:24 +0200

    a couple of comments

--- index.js
+++ index.js
@@ -10,11 +10,24 @@ const n_choose_k = function (n, k) {
     return factorial(n) / (factorial(n - k) * factorial(k))
 }
 
-
+/**
+ * 
+ * @param {*} n number of trials 
+ * @param {*} k numbers of successes
+ * @param {*} p percentage chance of success
+ * @returns Number
+ */
 const binomialDistributionFunction = function (n, k, p) {
     return n_choose_k(n, k) * p ** k * (1 - p) ** (n - k)
 }
 
+/**
+ * 
+ * @param {*} n number of trials 
+ * @param {*} k up to numbers of successes
+ * @param {*} p percentage chance of success
+ * @returns Number
+ */
 const binomialDistributionFunctionCumulative = function (n, k, p) {
     let total = 0;
     for(let i = 0; i <= k; i++) {
